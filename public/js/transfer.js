d691265b141b33329df72539dc199f593732907a
Tien Nguyen <tiennguyenminh2102@gmail.com>
2016-10-29 11:41:15 +0700

    Add multiple files uploading feature

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -2,42 +2,66 @@ var socket = io();
 
 $(function () {
     var $imageSelector = $('#imageSelector');
-    var $previewImage = $('#preview');
     var $modal = $('#modal');
+    var $modalBody = $('.modal-body');
     var $save = $('#save');
     var $images = $('#images');
+    var count = 0;
 
     $imageSelector.on('change', function () {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            var blob = new Blob([e.target.result]);
-            $previewImage[0].src = URL.createObjectURL(blob);
-            $modal.modal('show');
-        };
         if ($imageSelector[0].files.length) {
-            reader.readAsArrayBuffer($imageSelector[0].files[0]);
+            for (var i = 0; i < $imageSelector[0].files.length; i++) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    count++;
+                    var blob = new Blob([e.target.result]);
+                    var src = URL.createObjectURL(blob);
+
+                    $modalBody.append('<div class="thumbnail preview"><img src="' + src + '" id="' + count + '"></div>');
+                };
+
+                reader.readAsArrayBuffer($imageSelector[0].files[i]);
+            }
+
+            $modal.modal('show');
             $imageSelector.val('');
-        } else {
-            console.log('No file chosen.');
+
         }
+    }).on('hidden.bs.modal', function () {
+        $modalBody.empty();
     });
 
-    var croppedCanvas;
+    var cropBoxData;
 
     $modal.on('shown.bs.modal', function () {
-        $previewImage.cropper({
+        var $preview = $('.preview > img');
+
+        $preview.cropper({
             autoCropArea: 1,
             aspectRatio: 1/1,
-            // cropBoxResizable: false,
-            // zoomable: false
+            multiple: true,
+            built: function () {
+                for (var i = 1; i <= count; i++) {
+                    $('#' + i).cropper('setCropBoxData', cropBoxData);
+                }
+            }
         });
+
     });
 
     $save.on('click', function () {
-        croppedCanvas = $previewImage.cropper('getCroppedCanvas');
-        var compressedCanvas = croppedCanvas.toDataURL('image/jpeg', 0.7);
-        socket.emit('sendPhoto', {base64: compressedCanvas});
-        $previewImage.cropper('destroy');
+
+        // cropBoxData = $('#1').cropper('getCropBoxData');
+        // canvasData = $('#1').cropper('getCanvasData');
+
+        for (var i = 1; i <= count; i++) {
+            var croppedCanvas = $('#' + i).cropper('getCroppedCanvas');
+            var compressedCanvas = croppedCanvas.toDataURL('image/jpeg', 0.7);
+            socket.emit('sendPhoto', {base64: compressedCanvas});
+        }
+
+        count = 0;
+        $modalBody.empty();
         $modal.modal('hide');
     });
 
